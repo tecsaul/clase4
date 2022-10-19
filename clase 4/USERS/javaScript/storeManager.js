6a854321b11c7ab9912c05125fca2803d1f12464
saul nuñez <77266273+tecsaul@users.noreply.github.com>
2022-10-19 00:06:19 -0700

    Add files via upload

---
+++ clase 4/USERS/javaScript/storeManager.js
@@ -0,0 +1,9 @@
+function saveUser(newUser) {
+    console.log(newUser);
+    let val = JSON.stringify(newUser);
+    console.log(val);
+    localStorage.setItem("user", val);
+}
+function readUser(){
+    
+}
